71a5b6b6d8a94e53d22aa516540092d4a6d0db55
kter <me@kter.jp>
2016-09-22 02:31:31 +0900

    ユニットテストモードを追加

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -6,6 +6,9 @@ if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   # シグナルがこのコマンドに伝わるよう、新しいプロセスが生成されないようにする
   exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "tests.py"
 else
   echo "Running Production Server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
